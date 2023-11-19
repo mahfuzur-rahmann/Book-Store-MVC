ed9463dae6ab0b41993634ba33c200cc6b4d8bed
Mahfuzur Rahman <mahfuzzz.cse@gmail.com>
2023-11-20 00:54:56 +0600

    Company controller added & CRUD configured

--- BookStore.Models/Company.cs
+++ BookStore.Models/Company.cs
@@ -15,10 +15,17 @@ namespace BookStore.Models
         [Required]
         public string  Name { get; set; }
 
+        [Display(Name = "Street Address")]
+
         public string?  StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
+
+        [Display(Name = "Postal code")]    
         public string? PostalCode { get; set; }
+
+        [Display(Name = "Phone number ")]
+
         public string? PhoneNumber { get; set; }
     }
 }
