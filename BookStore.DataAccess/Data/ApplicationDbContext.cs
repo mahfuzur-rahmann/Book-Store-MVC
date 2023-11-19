95ede8ce7baba6cd1475da70cabb3fbf4b9a896f
Mahfuzur Rahman <mahfuzzz.cse@gmail.com>
2023-11-19 23:59:23 +0600

    Company model created and set up repository & unitOfWork

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -38,8 +38,9 @@ namespace BookStore.DataAccess.Data
         }
 
 
-        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Category> Company { get; set; } 
         public DbSet<Product>  Products { get; set; }
+        public DbSet<Company>   Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
