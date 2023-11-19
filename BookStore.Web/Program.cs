82052ecbb6fdbd53c23f80bdd95733d7095ea7ec
Mahfuzur Rahman <mahfuzzz.cse@gmail.com>
2023-11-19 15:44:02 +0600

    Configured DbContext

--- BookStore.Web/Program.cs
+++ BookStore.Web/Program.cs
@@ -1,7 +1,12 @@
+using BookStore.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabaseConnectionString")));
 
 var app = builder.Build();
 
