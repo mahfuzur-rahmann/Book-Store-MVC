2532cb1f0033fb6198c1cd5feaf40d6d3d812bbd
Mahfuzur Rahman <mahfuzzz.cse@gmail.com>
2023-11-19 16:59:04 +0600

    Repository pattern & UnitOfWork added to the projects

---
+++ BookStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(string? includePrperties = null);
+        T Get(Expression<Func<T, bool>> expression, string? includePrperties = null);
+
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
---
+++ BookStore.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+
+
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Product productObject)
+        {
+            var objFromDb = _db.Products.FirstOrDefault(p => p.Id == productObject.Id);
+            if (objFromDb! != null)
+            {
+                objFromDb.Title = productObject.Title;
+                objFromDb.ISBN = productObject.ISBN;
+                objFromDb.Author = productObject.Author;
+                objFromDb.Description = productObject.Description;
+                objFromDb.Price = productObject.Price;
+                objFromDb.Price50 = productObject.Price50;
+                objFromDb.Price100 = productObject.Price100;
+                objFromDb.ListPrice = productObject.ListPrice;
+                objFromDb.CategoryId = productObject.CategoryId;
+                if (productObject.ImgUrl != null)
+                {
+
+                    objFromDb.ImgUrl = productObject.ImgUrl;
+                }
+
+            }
+            //_db.Products.Update(productObject);
+
+        }
+    }
+}
