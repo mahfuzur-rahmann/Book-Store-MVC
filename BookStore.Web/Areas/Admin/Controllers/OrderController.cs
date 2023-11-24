c40f05610464e5ecb4bea10a5f35cea5d4c06fe3
Mahfuzur Rahman <mahfuzzz.cse@gmail.com>
2023-11-25 02:48:48 +0600

    Manage order display with toggle done

---
+++ BookStore.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,56 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStore.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookStore.Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+			 _unitOfWork = unitOfWork;
+
+		}
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll(string status)
+		{
+			IEnumerable<OrderHeader> orderHeaders;
+           
+            orderHeaders = _unitOfWork.OrderHeader.GetAll(includePrperties: "ApplicationUser");
+
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+            return Json(new { data = orderHeaders });
+
+		}
+		#endregion
+	}
+}
